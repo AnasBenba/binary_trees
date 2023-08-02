8c82d02dc552d177bfed2370e55acd571e46ac77
AnasBenba <anasbenba123@gmail.com>
2023-08-02 05:11:25 -0700

    fix code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,18 +18,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree->left != NULL)
 	{
-		left_height = binary_tree_height(tree->left);
+		left_height = 1 + binary_tree_height(tree->left);
 	}
 	if (tree->right != NULL)
 	{
-		right_height = binary_tree_height(tree->right);
+		right_height = 1 + binary_tree_height(tree->right);
 	}
 	if (left_height > right_height)
 	{
-		return (left_height + 1);
+		return (left_height);
 	}
 	else
 	{
-		return (right_height + 1);
+		return (right_height);
 	}
 }
